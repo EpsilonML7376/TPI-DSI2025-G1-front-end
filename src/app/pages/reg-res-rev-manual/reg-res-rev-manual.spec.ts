d9fcf5de10cc3ccd09f140741414f87d9ad70d45
Tomas Gambino <tomigambino21@gmail.com>
2025-11-15 12:40:10 -0300

    Agregue pages

---
+++ src/app/pages/reg-res-rev-manual/reg-res-rev-manual.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegResRevManual } from './reg-res-rev-manual';
+
+describe('RegResRevManual', () => {
+  let component: RegResRevManual;
+  let fixture: ComponentFixture<RegResRevManual>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RegResRevManual]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RegResRevManual);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/reg-res-rev-manual/reg-res-rev-manual.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reg-res-rev-manual',
+  imports: [],
+  templateUrl: './reg-res-rev-manual.html',
+  styleUrl: './reg-res-rev-manual.css'
+})
+export class RegResRevManual {
+
+}
