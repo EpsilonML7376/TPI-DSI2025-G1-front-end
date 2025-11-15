f247f927ec8ff47094ba95aa0a61d33566a1f5e3
Tomas Gambino <tomigambino21@gmail.com>
2025-11-15 13:15:46 -0300

    Implementación del service

---
+++ src/app/service/service-es.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceES {
+
+  constructor(private http: HttpClient) { }
+
+  getESNoRevisados(): Observable<String[]> {
+    return this.http.get<String[]>('http://localhost:8080/norevisados');
+  }
+
+  postSelectEvent(event:string): Observable<any> {
+    return this.http.post('http://localhost:8080/seleccion-resultado', { event })
+  }
+
+  postSelectResult(selection:string, evento: string): Observable<any> {
+      return this.http.post('http://localhost:8080/seleccion-resultado', { selection, evento })
+    }
+}
